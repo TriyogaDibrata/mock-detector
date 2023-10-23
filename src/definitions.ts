6c01800adf7ad53d8a32b158bb2d944cf0379439
triyogadibrata <triyoga.dibrata@gmail.com>
2023-10-23 17:00:48 +0800

    #dev remove echo plugin

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,7 +1,6 @@
 import type { Plugin } from '@capacitor/core';
 
 export interface MockDetectorPlugin extends Plugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
   detectMock(): Promise<MockDetectorResult>;
 }
 
--- src/web.ts
+++ src/web.ts
@@ -3,11 +3,6 @@ import { WebPlugin } from '@capacitor/core';
 import type { MockDetectorPlugin, MockDetectorResult } from './definitions';
 
 export class MockDetectorWeb extends WebPlugin implements MockDetectorPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
-  }
-
   async detectMock(): Promise<MockDetectorResult> {
     return {
       isMock: false,
